d4d6ea0c2d591ad40fd3d947e8a74e75246e5fd0
RobesNerd <felipecarvalhotauil@gmail.com>
2024-06-11 21:47:42 -0300

    main

--- src/JavaStudy.java
+++ src/JavaStudy.java
@@ -3,16 +3,41 @@ import java.util.Scanner;
 public class JavaStudy {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("How many numbers for your operation:");
+        System.out.print("How many numbers for your operation: ");
         int a = scanner.nextInt();
-        int[] number = new int[a];;
+        int[] number = new int[a];
         for (int i = 0; i < a; i++) {
-            System.out.println("Choose a number:");
-            int b = scanner.nextInt();
-            number[i] = b;
-            System.out.println(number);
-            scanner.close()
+            System.out.print("Choose a number: ");
+            number[i] = scanner.nextInt();
         }
-
+        System.out.print("Choose any operator (+, -, /, *): ");
+        String b = scanner.next();
+        if (b.equals("*")) {
+            int c = 1;
+            for (int i = 0; i < a; i++) {
+                c *= number[i];
+            }
+            System.out.print(c);
+        } else if (b.equals("+")) {
+            int c = 0;
+            for (int i = 0; i < a; i++) {
+                c += number[i];
+            }
+            System.out.print(c);
+        } else if (b.equals("-")) {
+            int c = 0;
+            for (int i = 0; i < a; i++) {
+                c -= number[i];
+            }
+            System.out.print(c);
+        } else if (b.equals("/")) {
+            int c = 1;
+            for (int i = 0; i < a; i++) {
+                c /= number[i];
+            }
+            System.out.print(c);
+        } else {
+            System.out.print("Error!");
+        }
     }
 }
